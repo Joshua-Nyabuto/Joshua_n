4b75aea4d13baa6fd3d432d23d9f34babc3d1581
Joshua-Nyabuto <137417790+Joshua-Nyabuto@users.noreply.github.com>
2023-07-24 16:58:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Portfolio</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Welcome to My Portfolio</h1>
+  </header>
+
+  <section id="hero">
+    <div class="hero-content">
+      <h2>Hello, I am Joshua Nyabuto </h2>
+      <p>A web developer with a passion for creating beautiful and functional websites and web-based systems</p>
+    </div>
+  </section>
+
+   <section id="projects">
+    <h2>My Projects</h2>
+    <div class="project">
+   <img src="images/pic2.png"  style="width:400px; height:300px" alt="Project 1" width="200" height="200">
+  <h3>VISITORS MANAGEMENT SYSTEM</h3>
+  <p>
+    The Visitors Management System is a web-based application designed to streamline and enhance the process of managing visitors in various settings, such as offices, schools, or events.
+
+    The system allows administrators to efficiently register and track visitors, ensuring a smooth check-in and check-out process. Visitors can provide their details through a user-friendly interface, and the system captures important information like name, contact details, purpose of visit, and check-in time.
+
+    Key features of the Visitors Management System include real-time visitor logging, badge printing, visitor history tracking, and automated email notifications to hosts upon visitor arrival. The system also offers security enhancements by providing a centralized database of visitors, which can be helpful for emergency situations or compliance requirements.
+
+    As the specialist in web-based system development, I spearheaded the entire development lifecycle of the Visitors Management System. I leveraged my expertise in front-end and back-end technologies to create an intuitive and responsive user interface. The back-end was implemented using PHP and MySQL, ensuring secure data storage and retrieval.
+
+    Throughout the development process, I collaborated closely with stakeholders to understand their specific needs and incorporated their feedback to tailor the system to their requirements. The Visitors Management System was successfully deployed and received positive feedback for its ease of use and efficiency in managing visitors effectively.
+
+    Technologies used: HTML, CSS, JavaScript, PHP, MySQL
+  </p>
+</div>
+
+    <div class="project">
+      <img src="images/project2.jpg" alt="Project 2">
+      <h3>Project 2</h3>
+      <p>Description of Project 2.</p>
+    </div>
+    <!-- Add more project items as needed -->
+  </section>
+
+  <section id="about">
+  <h2>About Me</h2>
+  <p>
+    Hello! I am Joshua Nyabuto, a computer scientist with expertise in web development,
+    web-based system development, android app development, networking, and providing computer user support.
+    I hold a degree in Computer Science and have a passion for leveraging technology to create innovative solutions.
+
+    My journey in the field of computer science started with a deep fascination for computers and technology. 
+    Over the years, I have honed my skills in web development, creating stunning and functional websites 
+    that offer seamless user experiences. I am proficient in HTML, CSS, JavaScript, and various front-end frameworks.
+
+    Additionally, I have a strong background in back-end development, building web-based systems that handle data 
+    processing and user interactions. I am experienced in using PHP, MySQL, and other server-side technologies.
+
+    My expertise extends to mobile app development, particularly in creating Android applications that cater to 
+    diverse user needs. I have experience in Java and Kotlin programming languages for Android development.
+
+    Apart from software development, I have a solid understanding of networking principles, which enables me 
+    to design and maintain efficient and secure network infrastructures.
+
+    As a computer scientist, I am passionate about helping others and providing exceptional support to computer users. 
+    I believe in the power of technology to transform businesses and improve people's lives.
+
+    Whether it's creating a website, developing a web-based system, crafting a mobile app, setting up networks, 
+    or assisting users with their computer-related needs, I am always eager to take on new challenges and deliver 
+    outstanding results.
+
+    Thank you for visiting my portfolio, and I look forward to collaborating on exciting projects and ventures.
+  </p>
+</section>
+
+
+  <footer>
+     <h2>CONTACT ME:</h2>
+    <p>Contact me at:nyabutojoshua30@gmail.com</p><br>
+    <p>Phone Number: 0792238891</p>
+  </footer>
+</body>
+</html>
